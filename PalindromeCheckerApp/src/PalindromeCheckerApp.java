3d5d93dfd4fe87ce345e6987f1a1935c2126704d
aarushisarkar <aarushisarkar18@gmail.com>
2026-03-02 09:56:57 +0530

    UC2: Hardcoded palidrome check

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input: " + input);
+
+        if (isPalindrome) {
+            System.out.println("It is a Palindrome: true");
+        } else {
+            System.out.println("It is a Palindrome: false");
+        }
+    }
+}
